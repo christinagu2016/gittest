36e39af9ed02bfe15aaa98556c0e923692e50ed8
christinagu <c_gu@yahoo.com>
2016-10-29 13:10:36 -0400

    autobuild push test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello Everyone, World");
+        System.out.println("Hello, this is a push test");
     }
 
 }
