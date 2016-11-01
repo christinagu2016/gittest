5121aa709be2ea5c1f357b621151dc47b15b3c8a
christinagu <c_gu@yahoo.com>
2016-10-31 23:05:24 -0400

    2nd time ssh test without .bashrc file

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, this is a ssh test");
+        System.out.println("Hello, this is 2nd ssh test");
     }
 
 }
